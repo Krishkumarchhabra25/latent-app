ff097f7a48f488ea538da769ccb7c248e8a687f3
krishkumarchhabra <123287035+KrishKuChhabra@users.noreply.github.com>
2025-01-26 01:40:27 +0530

    signup endpoints acded and db calls

--- apps/http-backend/src/routes/v1/user.ts
+++ apps/http-backend/src/routes/v1/user.ts
@@ -1,23 +1,60 @@
 import { generateToken, verifyToken } from "authenticator";
 import {Router} from "express"
+import {client} from "@repo/db/clinet"
+import jwt from "jsonwebtoken"
+import { JWT_PASSWORD } from "../../config";
 
 const router: Router = Router();
 
-router.post("/signup",(req,res)=>{
-    const phoneNumber = req.body.phoneNumber;
-   const totp =  generateToken(phoneNumber + "SIGNUP");
+router.post("/signup", async (req,res)=>{
+    const number = req.body.phoneNumber;
+   const totp =  generateToken(number + "SIGNUP");
+
+ const user = await client.user.upsert({
+      where:{
+        number
+      },
+      create:{
+        number,
+        name:""
+    },
+    update:{
+
+    }
+   })
+
+   
+   if(process.env.NODE_ENV === "production"){
+
+   }
    res.json({
-      id:"1"    
+      id:user.id   
    })
 });
-router.post("/signup",(req,res)=>{
-    const phoneNumber = req.body.phoneNumber;
-    if(!verifyToken(phoneNumber + "SIGNUP", req.body.otp)){
+router.post("/signup",async (req,res)=>{
+    const number = req.body.phoneNumber;
+    const name = req.body.name;
+    if(!verifyToken(number + "SIGNUP", req.body.otp)){
         res.json({
             messgae:"Invalid token"
         })
         return
     }
+
+ const userId = await client.user.update({
+        where:{
+            number
+        },
+        data:{
+            name,
+            verified:true
+        }
+    });
+
+    const token = jwt.sign({
+        userId,
+
+    }, JWT_PASSWORD)
 // set user to verified to DB
 
 });
